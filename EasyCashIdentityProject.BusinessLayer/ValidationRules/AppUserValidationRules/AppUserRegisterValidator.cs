553016ae7c440831f204e6621bd5e43c992915e1
İbrahim BEKTAŞ <brahim BEKTA@IBRAHIMBEKTAS>
2024-10-21 10:48:59 +0300

    Add project files.

---
+++ EasyCashIdentityProject.BusinessLayer/ValidationRules/AppUserValidationRules/AppUserRegisterValidator.cs
@@ -0,0 +1,27 @@
+﻿using EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.BusinessLayer.ValidationRules.AppUserValidationRules
+{
+    public class AppUserRegisterValidator:AbstractValidator<AppUserRegisterDto>
+    {
+        public AppUserRegisterValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Ad alanı boş geçilemez");
+            RuleFor(x => x.Surname).NotEmpty().WithMessage("Soyad alanı boş geçilemez");
+            RuleFor(x => x.Username).NotEmpty().WithMessage("Kullanıcı alanı boş geçilemez");
+            RuleFor(x => x.Email).NotEmpty().WithMessage("Email alanı boş geçilemez");
+            RuleFor(x => x.Password).NotEmpty().WithMessage("Şifre alanı boş geçilemez");
+            RuleFor(x => x.ConfirmPassword).NotEmpty().WithMessage("Şifre Tekrar alanı boş geçilemez");
+            RuleFor(x => x.Name).MaximumLength(30).WithMessage("En fazla 30 karekter girilebilir");
+            RuleFor(x => x.Name).MinimumLength(2).WithMessage("En az 3 karekterlik veri girmeniz gerekli");
+            RuleFor(x => x.ConfirmPassword).Equal(y => y.Password).WithMessage("Parolalar eşleşmedi");
+            RuleFor(x => x.Email).EmailAddress().WithMessage("Geçerli bir email adresi giriniz");
+        }
+    }
+}
---
+++ EasyCashIdentityProject.DtoLayer/Dtos/AppUserDtos/AppUserRegisterDto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos
+{
+    public class AppUserRegisterDto
+    {
+        //[Required(ErrorMessage ="Ad Alanı Zorunludur")]
+        //[Display(Name ="İsim")]
+        //[MaxLength(30,ErrorMessage ="En fazla 30 karekter girbilirsiniz")]
+        public string Name;
+        public string Surname;
+        public string Email;
+        public string Username;
+        public string Password;
+        public string ConfirmPassword;
+    }
+}
