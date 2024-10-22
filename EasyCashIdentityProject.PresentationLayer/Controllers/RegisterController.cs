c1912b2e0c7e2b32db0a4c629908f696b3d817a2
İbrahim BEKTAŞ <brahim BEKTA@IBRAHIMBEKTAS>
2024-10-22 09:28:00 +0300

    15. Video Sonu

--- EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
+++ EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
@@ -67,6 +67,8 @@ namespace EasyCashIdentityProject.PresentationLayer.Controllers
 					client.Send(mimeMessage);
                     client.Disconnect(true);
 
+                    TempData["Mail"] = appUserRegisterDto.Email;
+
                     return RedirectToAction("Index", "ConfirmMail");
                 }
                 else
